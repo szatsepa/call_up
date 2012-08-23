27c8f2c8d1b8bb2c19541748c6bd968fa7460cd5
szatsepa <arcady.1254@gmail.com>
2012-08-23 22:53:30 +0300

    PAGER ABC

--- exp/main/dsp_pager.php
+++ exp/main/dsp_pager.php
@@ -1,5 +1,5 @@
 <?php
-
+$simbl_array = array('O','A','B','C');
 if($pages >1){
 ?>
 <div class="pager">
@@ -13,16 +13,16 @@ if($pages >1){
         <a href="<?php echo "index.php?act=look&page=".($page-1).$address;?>" style="font-size: 16px;color:#990033;" onmouseover="this.style.color='#878787'" onmouseout="this.style.color='#990033'">Предыдущая&nbsp;</a>
 <?php
  }
-for($i=($page-1);$i<($page+3);$i++){
+for($i=($page-1);$i<($page+2);$i++){ 
 
-    if($i == $pages)break;
+    if($i == ($pages) or  $page == $pages)break;
     
 	$p = $i+1;
         
         ?>
 
     
-        <a href="<?php echo "index.php?act=look&page=$p$address";?>" style="font-size: 16px;color:#990033;" onmouseover="this.style.color='#878787'" onmouseout="this.style.color='#990033'"><?php echo $p;?></a>
+        <a href="<?php echo "index.php?act=look&page=$p$address";?>" style="font-size: 16px;color:#990033;" onmouseover="this.style.color='#878787'" onmouseout="this.style.color='#990033'"><?php echo $simbl_array[$p];?></a>
     
 
 
@@ -42,7 +42,7 @@ for($i=($page-1);$i<($page+3);$i++){
 
 </div>
 <div class="pager_0">  
-    <p class="pager_2" align="center">Страница <?php echo "$page из $pages";?>&nbsp;&nbsp;</p>
+    <p class="pager_2" style="font-size: 18px;font-weight: bold;" align="center">Страница <?php echo $simbl_array[$page];?>&nbsp;&nbsp;</p>
 </div>
             
 <?php
